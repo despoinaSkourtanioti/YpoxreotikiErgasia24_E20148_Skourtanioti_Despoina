48c232a11ba3d24cf712ad8ac2694c09ee9cf6d5
despoinaSkourtanioti <137726116+despoinaSkourtanioti@users.noreply.github.com>
2024-06-20 19:15:35 +0300

    Update config.py

--- hospital_management/app/config.py
+++ hospital_management/app/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://despoinaskourtanioti:<password>@cluster0.3vktqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://despoinaskourtanioti:12345@cluster0.3vktqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
